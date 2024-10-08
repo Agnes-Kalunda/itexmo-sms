f09a03c38ccbf116fb9cd87643c1daffd840c5e4
Agnes <agypeter97@gmail.com>
2024-10-08 18:18:26 +0300

    testing broadcast endpoint

--- test.php
+++ test.php
@@ -14,6 +14,11 @@ $config = [
 // instance of itexmo class
 $itexmoSms = new ItexmoSms($config);
 
+echo "Testing Broadcast..\n";
+
+$response = $itexmoSms->broadcast(['12345678909'], 'Test message from sample');
+print_r($response);
+
 
 
 
