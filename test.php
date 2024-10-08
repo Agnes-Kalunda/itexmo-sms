b16cc10fe426c3fc83c766cee11144198f517411
Agnes <agypeter97@gmail.com>
2024-10-08 18:22:07 +0300

    test for broadcast2d endpoint

--- test.php
+++ test.php
@@ -20,6 +20,17 @@ $response = $itexmoSms->broadcast(['12345678909'], 'Test message from sample');
 print_r($response);
 
 
+// broadcast2d endpoint test
+echo "\nTesting broadcast2d.....\n";
+$message = [
+    ['Recipient' => '12345678909', 'Message' => 'Message 1'],
+    ['Recipient' => '12345678909', 'Message' => 'Message 1']
+];
+
+$response = $itexmoSms->broadcast2d($message);
+print_r($response);
+
+
 
 
 ?>
