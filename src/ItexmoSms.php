9244f368da34c9dc8257b8312e3f17870cfde2c5
Agnes <agypeter97@gmail.com>
2024-10-08 13:43:04 +0300

    sendRequest method to handle API call for broadcasting messages

--- src/ItexmoSms.php
+++ src/ItexmoSms.php
@@ -13,8 +13,26 @@ class ItexmoSms{
             'base_uri'=> 'https://api.itexmo.com/api/',
             'timeout'=>10.0,
         ]);
+
+    }
+
+    public function broadcast(array $recipients, string $message, ?string $senderId =null){
+        $payload = [
+            'Email'=> $this->config['email'],
+            'password' => $this->config['password'],
+            'ApiCode' => $this->config['api_code'],
+            'Recipients' => json_encode($recipients),
+            'Message' => $message,
+        ];
+
+        if ($senderId){
+            $payload['SenderId'] = $senderId;
+        }
+
+        return $this->sendRequest('broadcast', $payload);
     }
     
+
 }
 
 
