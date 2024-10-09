a823b384bb740cbe5075167e10d4a680e2dbfdab
Agnes <agypeter97@gmail.com>
2024-10-09 17:47:36 +0300

    implement tests to test multiple api responses

--- tests/ItexmoSmsTest.php
+++ tests/ItexmoSmsTest.php
@@ -4,8 +4,6 @@ namespace Agnes\ItexmoSms\Tests;
 
 use Agnes\ItexmoSms\ItexmoSms;
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Exception\RequestException;
 use PHPUnit\Framework\TestCase;
@@ -13,22 +11,15 @@ use Psr\Http\Message\RequestInterface;
 
 class ItexmoSmsTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
     public function testBroadcastSuccess()
     {
-        $mock = new MockHandler([
-            new Response(200, [], json_encode(['status' => 'SUCCESS']))
-        ]);
-
-        $handlerStack = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handlerStack]);
+        /** @var Client|\PHPUnit\Framework\MockObject\MockObject $clientMock */
+        $clientMock = $this->createMock(Client::class);
+        $clientMock->method('post')
+            ->willReturn(new Response(200, [], json_encode(['status' => 'SUCCESS'])));
 
         $itexmo = new ItexmoSms(['api_code' => 'test_api_code']);
-        $itexmo->setClient($client);
+        $itexmo->setClient($clientMock);
 
         $response = $itexmo->broadcast(['1234567890'], 'Test message');
         $this->assertEquals('SUCCESS', $response['status']);
@@ -36,15 +27,13 @@ class ItexmoSmsTest extends TestCase
 
     public function testUnauthorizedError()
     {
-        $mock = new MockHandler([
-            new Response(401, [], null)
-        ]);
-
-        $handlerStack = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handlerStack]);
+        /** @var Client|\PHPUnit\Framework\MockObject\MockObject $clientMock */
+        $clientMock = $this->createMock(Client::class);
+        $clientMock->method('post')
+            ->willReturn(new Response(401, [], null));
 
         $itexmo = new ItexmoSms(['api_code' => 'test_api_code']);
-        $itexmo->setClient($client);
+        $itexmo->setClient($clientMock);
 
         $response = $itexmo->broadcast(['1234567890'], 'Test message');
         $this->assertNull($response);
@@ -52,19 +41,21 @@ class ItexmoSmsTest extends TestCase
 
     public function testNetworkError()
     {
-        $mock = new MockHandler([
-            new RequestException('Network error', $this->createMock(RequestInterface::class))
-        ]);
-
-        $handlerStack = HandlerStack::create($mock);
-        $client = new Client(['handler' => $handlerStack]);
+        /** @var Client|\PHPUnit\Framework\MockObject\MockObject $clientMock */
+        $clientMock = $this->createMock(Client::class);
+        /** @var RequestInterface|\PHPUnit\Framework\MockObject\MockObject $requestMock */
+        $requestMock = $this->createMock(RequestInterface::class);
+        $clientMock->method('post')
+            ->willThrowException(new RequestException('Network error', $requestMock));
 
         $itexmo = new ItexmoSms(['api_code' => 'test_api_code']);
-        $itexmo->setClient($client);
+        $itexmo->setClient($clientMock);
 
         $this->expectException(\RuntimeException::class);
         $this->expectExceptionMessage('HTTP request error');
 
         $itexmo->broadcast(['1234567890'], 'Test message');
     }
+
+    
 }
