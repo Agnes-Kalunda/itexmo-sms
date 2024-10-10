6f5c6b937805bfca2d88f77cda4ceb0748a6bc09
Agnes <agypeter97@gmail.com>
2024-10-10 22:36:27 +0300

    refactored

--- tests/ItexmoSmsTest.php
+++ tests/ItexmoSmsTest.php
@@ -74,58 +74,19 @@ class ItexmoSmsTest extends TestCase
 
             $this->itexmo->setClient($client);
 
-        $response = $this->itexmo->broadcast(['invalid_number'], 'Test message');
-        $this->assertEquals([
-            'success'=> false,
-            'message'=> 'Invalid Number',
-            'data'=> ['status'=> 1],
-        ], $response);
+            $response = call_user_func_array([$this->itexmo, $method], $args);
+            $this->assertEquals([
+                'success' => false,
+                'message' => $errorMessages[$code],
+                'data' => ['status' => $code]
+            ], $response);
+        }
     }
 
-
-    // test case for broadcast2d messaging
-
-    public function testBroadcast2dSuccessful(){
-        $mock = new MockHandler([
-            new Response(200, [], json_encode(['status'=> 0]))
-        ]);
-
-        $handlerStack = HandlerStack::create($mock);
-        $client = new Client([''=> $handlerStack]);
-        $this->itexmo->setClient($client);
-
-        $messages =[
-            ['1234567890', 'Message 1'],
-            ['1234567891', 'Message 2']
-        ];
-
-        $response = $this->itexmo->broadcast2d($messages);
-        $this->assertEquals([
-            'success'=> true,
-            'message'=> 'Message sent successfully',
-            'data'=> ['status'=> 0]
-        ], $response);
-    }
-
-
-    public function testBroadcastOtpSuccessful(){
-        $mock = new MockHandler([
-            new Response(200, [], json_encode(['status'=>0]))
-            ]);
-
-        $handlerStack = HandlerStack::create($mock);
-        $client = new Client(['handler'=> $handlerStack]);
-        $this->itexmo->setClient($client);
-
-        $response = $this->itexmo->broadcastOtp('12345678909', 'Your OTP is 123456');
-        $this->assertEquals([
-            'success'=> true,
-            'message'=> 'Message sent successfully',
-            'data'=> ['status'=> 0]
-        ], $response);
-    }
-
-    public function testInsufficientCreditError()
+    /**
+     * @dataProvider endpointProvider
+     */
+    public function testUnauthorizedRequest($method, $args)
     {
         $mock = new MockHandler([
             new Response(401, [])
@@ -197,4 +158,5 @@ class ItexmoSmsTest extends TestCase
             'query' => ['query', ['balance']],
         ];
     }
+
 }
