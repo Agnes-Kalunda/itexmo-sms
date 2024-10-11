f5b9d3c4bfd811d39614af7dab36aadb74560828
Agnes <agypeter97@gmail.com>
2024-10-11 19:16:54 +0300

    refactored

--- config/itexmo.php
+++ config/itexmo.php
@@ -2,7 +2,7 @@
 
 
 return [
-    // mandatory
+    // mandatoryy
     'email' => env('ITEXMO_EMAIL', ''),
     'password' => env('ITEXMO_PASSWORD', ''),
     'api_code' => env('ITEXMO_API_CODE', ''),
